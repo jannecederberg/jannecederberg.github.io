c31c4d9c8b1818923b3abb0ed6ec0791f5979921
Janne Cederberg <janne.cederberg@gmail.com>
2015-10-08 15:07:08 +0300

    Alter structure, add possibility for multiple rotated header images (header-img-extras)

---
+++ js/rotate-header.js
@@ -0,0 +1,22 @@
+var header_images = $('.intro-header').data('images');
+var header_list   = header_images.split(',');
+var header_pos    = 0;
+var header_path   = '/img/headers/';
+var header_rotate_interval = 5000;
+
+function setHeader(img) {
+	$('.intro-header').css({'background-image': 'url(/img/headers/'+img.trim()+')'});
+}
+
+$.fn.preload = function() {
+    this.each(function(){
+        $('<img/>')[0].src = header_path + this.trim();
+    });
+}
+
+$(header_list).preload();
+
+setInterval(function() {
+	header_pos = (header_pos+1) % header_list.length;
+	setHeader(header_list[header_pos].trim());
+}, header_rotate_interval);
